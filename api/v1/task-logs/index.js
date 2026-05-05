c387ddd886f83a6f7e4ad198a7ec4a9f81567ac5
Pankaj Sharma <pancage.learning@gmail.com>
2026-05-05 22:59:45 +0530

    remove old api/v1 folder, use single handler

--- api/v1/task-logs/index.js
+++
@@ -1,9 +0,0 @@
-const { setCorsHeaders, withAuth } = require('../../_lib/adapter')
-const { logTask } = require('../../../backend/controllers/taskLogsController')
-
-module.exports = async function handler(req, res) {
-  setCorsHeaders(res)
-  if (req.method === 'OPTIONS') return res.status(200).end()
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' })
-  return withAuth(req, res, logTask)
-}
